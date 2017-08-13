2915afb1368e3c89818c3b2a65cbfbcbbacbce03
Daniel Escobar <escobard90@gmail.com>
2017-08-13 16:21:50 -0600

    doc: added `README.md`

---
+++ README.md
@@ -0,0 +1,9 @@
+## Advanced React & Redux
+
+### Purpose
+This repository is created to keep track of my progress of Stephen Grider's `Advanced React & Redux` course. 
+
+Ideally these mini-projects will be expanded into portfolio pieces in the future. 
+
+### Contribution && Licensing
+Feel free to utilize the files in this repository as you see fit :muscle:
--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,14 +1,12 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use descripe to group together similar tests
+// The string in here is rendered within the console
+describe('App');
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+// describes one specific test
+it
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+// this method 
+expect
