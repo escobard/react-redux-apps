5c4fb0888df565818baa2a42c22460454069656c
Daniel Escobar <escobard90@gmail.com>
2017-08-13 18:18:54 -0600

    feat: section 2 lecture 5 notes

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -3,10 +3,30 @@ import App from '../../src/components/app';
 
 // Use descripe to group together similar tests
 // The string in here is rendered within the console
-describe('App');
+describe('Tests app component', () => {
 
-// describes one specific test
-it
+	// describes one attribute of this component that is being tested
+	it('shows the correct text', () => {
+		/*
+		// this method expects a specific attribute of the tested component to be true
+		expect
+		// this is the component being tested
+		(component)
+		// this is the boolean that ties the component to the expect
+		.to.have.class
+		// this is the value that we expect
+		('comment-box');
+		*/
 
-// this method 
-expect
+		// utilizes stephen griders method to render react components - this is REALLY useful for eact applications that require props
+		const component = renderComponent(App);
+
+		// uses expect to make an assertion about a specific component
+		expect(component).to.contain('React simple starter');
+	
+	});
+
+
+
+});
+
