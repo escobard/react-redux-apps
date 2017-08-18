89cfc3faabe8b182f6117bd10f7f9a135f56a247
Daniel Escobar <escobard90@gmail.com>
2017-08-18 02:14:10 -0600

    feat: section 2 lesson 18

--- testing/test/components/CommentList_test.js
+++ testing/test/components/CommentList_test.js
@@ -3,8 +3,20 @@ import CommentList from '../../src/components/CommentList';
 
 describe('Tests CommentList component', () => {
 
-	it('', () => {
-
-	})
+	let component;
 	
+	beforeEach(() =>{
+		const props = { comments: ['New Comment', 'New Comment 2']};
+		component = renderComponent(CommentList, null, props);
+	})
+
+	it('shows an LI for each comment', () => {
+		expect(component.find('li').length).to.equal(2);
+	})
+
+	it('shows each comment provided', () => {
+		// multiple expect methods can be utilized to test components
+		expect(component).to.contain('New Comment');
+		expect(component).to.contain('New Comment 2');
+	})
 })
