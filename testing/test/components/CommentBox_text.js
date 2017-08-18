457a5297fd27857b85ebd61f36ad0b3ee04996da
Daniel Escobar <escobard90@gmail.com>
2017-08-17 21:07:15 -0600

    feat: section 2 lesson 11

--- testing/test/components/CommentBox_text.js
+++ testing/test/components/CommentBox_text.js
@@ -1,25 +1,21 @@
 import { renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/CommentBox';
 
+// can find the chai-jquery assertions here: https://github.com/chaijs/chai-jquery
 describe('Tests CommentBox component', () => {
+	// renders the component with props, utilizing a `redux` store
+	let component = renderComponent(CommentBox);
 
 	it('has a text area', () => {
-
-		// renders the component with props, utilizing a `redux` store
-		const component = renderComponent(CommentBox);
-
 		// uses expect to make an assertion about a specific component
-		expect(component).to.contain('React simple starter');
+		expect(component.find('textarea')).to.exist;
 	});
 
 	it('has a button', () => {
-
-		// renders the component with props, utilizing a `redux` store
-		const component = renderComponent(CommentBox);
-
-		// uses expect to make an assertion about a specific component
-		expect(component).to.contain('React simple starter');
+		expect(component.find('button')).to.exist;
 	});
-
+	it('has the correct class', () => {
+		expect(component).to.have.class('comment-box');
+	});
 });
 
