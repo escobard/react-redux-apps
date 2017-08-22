ca4f20588965704710d804038192f1427fd8ca08
Daniel Escobar <escobard90@gmail.com>
2017-08-21 19:26:49 -0600

    feat: section 2 lecture 22

--- testing/src/reducers/comments.js
+++ testing/src/reducers/comments.js
@@ -1,3 +1,12 @@
-export default function (state=[], actions) {
-	
+import { SAVE_COMMENT } from '../actions/types';
+
+export default function (state=[], action) {
+	switch(action.type) {
+		case SAVE_COMMENT:
+			// the ES5 equivalent of this is state.concat([action.payload])
+			// this adds the payload onto the state array
+			return [ ...state, action.payload ];
+
+	}
+	return state;	
 }
