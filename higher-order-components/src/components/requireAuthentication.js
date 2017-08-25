fd67e47eacb29fc8a80acb4485c4f2f2cf194bba
Daniel Escobar <escobard90@gmail.com>
2017-08-25 02:20:56 -0600

    feat: section 4 lecture 42

--- higher-order-components/src/components/requireAuthentication.js
+++ higher-order-components/src/components/requireAuthentication.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 
 export default function(ComposedComponent){
-	
+
+	class Authentication extends Component{
+		render(){
+			return <ComposedComponent {..this.props} />
+		}
+	}
+
+	return Authentication;
 }
 
+/* 
+
+TO use this HOC
+
+import Authentication // this is the HOC
+import Resources // this is the component I want to wrap
+
+const ComposedComponent = Authentication(Resources);
+
+to utilize the composed component within a render method:
+
+<ComposedComponent resources={resourceList}/>
+
+IMPORTANT: A HIGHER ORDER COMPONENT IS A FUNCTION THAT IS CALLED WITHIN AN EXSISTING COMPONENT 
+
+The {...this.props} line passes props from the child component (in this case Resources) via the ComposedComponent function
+
+*/
