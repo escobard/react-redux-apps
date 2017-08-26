c4536b100337c4396ef2b9c19a46d82be15794d0
Daniel Escobar <escobard90@gmail.com>
2017-08-25 22:52:59 -0600

    feat: section 5 lecture 58

--- middleware/src/middlewares/async.js
+++ middleware/src/middlewares/async.js
@@ -1,10 +1,30 @@
 // this is generally the main structure of a middleware function
+// a middleware is a function that does something with actions BEFORE they hit the reducers
 export default function({dispatch}){
 	return next => 
 		action => {
-			console.log(action);
 
-			next(action);
+			// if the action does not have a payload or the payload does not have a .then property
+			// we don't care, send it on
+			// its important to always add a condition for actions we do NOT want to target 
+			if (!action.payload || !action.payload.then) {
+				return next(action);
+			}
+
+			// makes sure the promise is resolved
+			action.payload.then(response =>{
+
+					// take whatever data our action contains, and replace the payload with the new response
+					// effectively replacing the promise with our response data
+					// the const here is unecessary, but added for clarity.
+					const newAction = {...action, payload: response};
+
+					// dispatches the action back to the beginning of the middleware chain
+					dispatch(newAction);
+				})
+				.catch(err =>{
+					console.log(err);
+				})
 		}
 }
 
