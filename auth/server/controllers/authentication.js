4ffd9d1fcf5ee5bea3ed917a0d34fe9479735fe2
Daniel Escobar <escobard90@gmail.com>
2017-09-03 17:07:37 -0600

    feat: section 6, lecture 71
    - create, save, check, and store user email and password from the API!

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -14,14 +14,40 @@ exports.signup = function(req, res, next){
 
 	// findOne is a method whos first argument is the search criteria we want to use
 	// second argument is a callback function, where we pass an error object, and the user that matches the search criteria
+
 	User.findOne({email: email}, function(err, existingUser){
 
+		if (err) { return next(err)};
+
+		// if  a user with email does exists, return an error
+		if (existingUser) {
+
+			// returns an HTTP response of 422 - unprossesable entity, couldnt process data
+			// send an an object with an error object, with a property
+			return res.status(422).send({error: 'Email is in use'});
+		}
+
+		// if a user with email does not exists create and save user record
+
+		// utilizes the user model to create a new user model with the new user properties
+		const user = new User({
+			email: email,
+			password: password
+		});
+
+		// respond to request indicating the user was created
+		// the .save() being part of the `mongoose` library
+		user.save(function(err){
+			if (err) {return next(err)};
+
+			res.json({success: true});
+		})
 	});
 	
 
-	// if  a user with email does exists, return an error
+	
 
-	// if a user with email does not exists create and save user record
+	
 
-	// respond to request indicating the user was created
+
 }
