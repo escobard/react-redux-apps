adbc69a10427c0a75bdcb2454235dd2bd81db9da
Daniel Escobar <escobard90@gmail.com>
2017-09-08 16:07:23 -0600

    feat: section 7 lecture 110

--- auth/client/src/components/form.js
+++ auth/client/src/components/form.js
@@ -1,18 +1,57 @@
 import React, {Component} from 'react';
 
 export default class Form extends Component{
+
 	constructor(props) {
 		super(props);
 		this.state = {
 			email :'',
 			password :'',
-			confirmpass: '',
+			confirmpass: ''
 		}
 
     	this.handleCredential= this.handleCredential.bind(this);
     	this.handleFormSubmit= this.handleFormSubmit.bind(this);
     	this.renderInputs = this.renderInputs.bind(this);
 	}
+
+	handleCredential(event){
+		let {id, value} = event.target;
+		switch(id){
+			case 'email':
+				return this.setState({email:value});
+			case 'password':
+				return this.setState({password: value});
+			case 'confirmpass':
+				return this.setState({confirmpass: value});
+		}
+	}
+
+	handleFormSubmit(event){
+		event.preventDefault();
+		let {email, password, confirmpass} = this.state;
+		this.props.handler(email, password, confirmpass);
+	}
+
+	handleFormError(error){
+		const errors = [];
+		let {email, password, confirmpass} = this.state;
+
+		// error handlers
+		// this should be pretified later, but for now this works fine
+		error ? errors.push(error): null;
+		let passError = password !== confirmpass ? errors.push('Make sure your passwords match'): null;
+		let emptyFields = this.props.formLabels.map((label, index) =>{
+			if (this.state[label] == '') {
+				errors.push(`${label} must be filled`);
+			}
+		})
+		return(
+			<span className="error-message col-md-12">{ errors.join(', ') }</span>
+		)
+
+	}
+
 	renderInputs(inputs){
 		return inputs.map((input, index) =>{
 			return(
@@ -20,43 +59,22 @@ export default class Form extends Component{
 				<fieldset key={index} className="form-group">
 					<label htmlFor={input}>{input}</label>
 					<input value={this.state[input]} onChange={this.handleCredential} 
-					type={input =='password'? 'password': 'text'} name={input} id={input} className="form-control"/>
+					type={input =='password' || input == 'confirmpass' ? 'password': 'text'} name={input} id={input} className="form-control"/>
 				</fieldset>
 
 			);
 		})
 	}
-	handleCredential(event){
-		let {id, value} = event.target;
-		switch(id){
-			case 'email':
-				this.setState({email:value});
-			case 'password':
-				this.setState({password: value});
-			case 'confirmpass':
-				this.setState({confirmpass: value});
-		}
-	}
-	handleFormSubmit(event){
-		let {email, password} = this.state;
-		event.preventDefault();
-		this.props.handler(email, password);
-	}
-	handleFormError(error){
-		return(
-			<span className="error-message col-md-12">{error ? error : 'Please enter your login credentials'}</span>
-		)
 
-	}
 	render(){
-		let {error, formLabels} = this.props
-		console.log(this.props);
+		let {error, formLabels, variant} = this.props
+		let buttonText = variant ? 'Sign Up' : 'Sign In'
 		return(
 
 			<form onSubmit={this.handleFormSubmit}>
 				{this.renderInputs(formLabels)}
 				{this.handleFormError(error)}
-				<button action="submit" className="btn btn-primary">Sign in</button>
+				<button action="submit" className="btn btn-primary">{buttonText}</button>
 			</form>
 
 		);
