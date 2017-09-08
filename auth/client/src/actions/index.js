cf6bca3319601ef79e048701ef8bec1e142c6ecc
Daniel Escobar <escobard90@gmail.com>
2017-09-08 00:20:48 -0600

    feat: section 7 lecture 96
    - CORS requests, and how browser requests to other domains are handled

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -5,12 +5,12 @@ export function signinUser(email, password){
 	let test = { email, password}
 	// with redux thunk we can return a function to access the dispatch method of `react-redux`
 	// this allows us to add logic before dispatching the action.object into the reducer
-	console.log(test);
 	
 	return function(dispatch){
 
 		// submit email/password to the server
-		axios.post(`${ROOT_URL}/${SIGN_IN}`, test)
+		axios.post(`${ROOT_URL}/${SIGN_IN}`, test);
+		
 		// if request is good..
 		// update state to indate user is authenticated
 		// save the jwt token
--- auth/client/src/actions/types.js
+++ auth/client/src/actions/types.js
@@ -3,10 +3,10 @@
 // basically I can use this base structure to create any kind of node.js API I create :)
 
 // change this to whatever port the local BE server is set to
-const ROOT_URL = 'http://localhost:3090';
+export const ROOT_URL = 'http://localhost:3090';
 
 // change this to whatever port the production BE server is set to
-const PROD_URL = 'http://localhost:3090';
+export const PROD_URL = 'http://localhost:3090';
 
 // sign in URL
-const SIGN_IN = 'signin';
+export const SIGN_IN = 'signin';
