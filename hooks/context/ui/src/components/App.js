2c2ab24fd7f96ff470dc0f4b28390a97d112b414
Daniel Escobar <escobard90@gmail.com>
2019-09-14 14:56:45 -0600

    context - added base elements

---
+++ hooks/context/ui/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class App extends React.Component {
+  state = { language: 'english'}
+
+  on
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
+        </div>
+      </div>
+  )
+  }
+}
+
+export default App;
