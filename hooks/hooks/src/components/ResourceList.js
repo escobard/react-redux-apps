a845d96891bc5dedad48cb797898c2e41f4c1b64
Daniel Escobar <escobard90@gmail.com>
2019-10-05 14:54:47 -0600

    hooks-effect - feat: list render update

--- hooks/hooks/src/components/ResourceList.js
+++ hooks/hooks/src/components/ResourceList.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
+  // request logic must be in a separate function, if request is being called by useEffect
   const fetchResource = async resource => {
     const response = await axios.get(
       `https://jsonplaceholder.typicode.com/${resource}`
@@ -13,17 +14,25 @@ const ResourceList = ({ resource }) => {
   };
 
   // useEffect replaces lifecycle methods, called every time the component is rendered
-  useEffect(() => {
-    // calls functions within the scope if props.resource's value changes
-    fetchResource(resource);
-  },
+  useEffect(
+    () => {
+      // calls functions within the scope if props.resource's value changes
+      fetchResource(resource);
+    },
     // removing the second argument of useEffect, calls the scoped function endlessly
     // adding an empty array as the second argument, calls the scoped function once on mount
     // can add multiple arguments, useEffect checks for changes on all arguments
     // on render if the props.resource value changes (from previous render), fetchResource is called
-    [resource]);
+    [resource]
+  );
 
-  return <div>{resources.length}</div>;
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default ResourceList;
